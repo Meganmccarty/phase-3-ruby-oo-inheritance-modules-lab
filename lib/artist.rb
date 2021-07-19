2708612de9a23a73223093ac6deea6d00aa5c106
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-19 15:03:12 -0400

    commit tree a9b918a186dd419754b41d9ef73a11a299bfc692

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,31 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name
-  attr_reader :songs
+    extend Memorable::ClassMethods
+    extend Findable::ClassMethods
+    include Paramable::InstanceMethods
+    include Memorable::InstanceMethods
 
-  @@artists = []
+    attr_accessor :name
+    attr_reader :songs
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+    @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+    def initialize
+        super
+        @songs = []
+    end
 
-  def self.all
-    @@artists
-  end
+    def self.all
+        @@artists
+    end
 
-  def self.reset_all
-    self.all.clear
-  end
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
 
-  def self.count
-    self.all.count
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+    def add_songs(songs)
+        songs.each { |song| add_song(song) }
+    end
 end
